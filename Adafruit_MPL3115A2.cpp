c762e6d3e794e6ebe77e28f38cc8caf7d261ab06
Quinten <tintin10q@hotmail.com>
2021-01-03 14:18:41 +0100

    Tried to fix formatting

--- Adafruit_MPL3115A2.cpp
+++ Adafruit_MPL3115A2.cpp
@@ -48,30 +48,30 @@ Adafruit_MPL3115A2::Adafruit_MPL3115A2() {}
  *   @return true on successful startup, false otherwise
  */
 boolean Adafruit_MPL3115A2::begin(TwoWire *twoWire) {
-  _i2c = twoWire;
-  _i2c->begin();
-  uint8_t whoami = read8(MPL3115A2_WHOAMI);
-  if (whoami != 0xC4) {
-    return false;
-  }
+    _i2c = twoWire;
+    _i2c->begin();
+    uint8_t whoami = read8(MPL3115A2_WHOAMI);
+    if (whoami != 0xC4) {
+        return false;
+    }
 
-  write8(MPL3115A2_CTRL_REG1, MPL3115A2_CTRL_REG1_RST);
-  delay(10);
-
-  while (read8(MPL3115A2_CTRL_REG1) & MPL3115A2_CTRL_REG1_RST)
+    write8(MPL3115A2_CTRL_REG1, MPL3115A2_CTRL_REG1_RST);
     delay(10);
 
-  _ctrl_reg1.reg = MPL3115A2_CTRL_REG1_OS128 | MPL3115A2_CTRL_REG1_ALT;
+    while (read8(MPL3115A2_CTRL_REG1) & MPL3115A2_CTRL_REG1_RST)
+        delay(10);
 
-  write8(MPL3115A2_CTRL_REG1, _ctrl_reg1.reg);
+    _ctrl_reg1.reg = MPL3115A2_CTRL_REG1_OS128 | MPL3115A2_CTRL_REG1_ALT;
+
+    write8(MPL3115A2_CTRL_REG1, _ctrl_reg1.reg);
 
 
-  write8(MPL3115A2_PT_DATA_CFG, 
-   MPL3115A2_PT_DATA_CFG_TDEFE |
-   MPL3115A2_PT_DATA_CFG_PDEFE |
-   MPL3115A2_PT_DATA_CFG_DREM);
-  state = 1;
-  return true;
+    write8(MPL3115A2_PT_DATA_CFG,
+           MPL3115A2_PT_DATA_CFG_TDEFE |
+           MPL3115A2_PT_DATA_CFG_PDEFE |
+           MPL3115A2_PT_DATA_CFG_DREM);
+    state = 1;
+    return true;
 }
 
 /*!
@@ -79,38 +79,38 @@ boolean Adafruit_MPL3115A2::begin(TwoWire *twoWire) {
  *  @return altitude reading as a floating point value
  */
 float Adafruit_MPL3115A2::getPressure() {
-  uint32_t pressure;
+    uint32_t pressure;
 
-  while (read8(MPL3115A2_CTRL_REG1) & MPL3115A2_CTRL_REG1_OST)
-    delay(10);
+    while (read8(MPL3115A2_CTRL_REG1) & MPL3115A2_CTRL_REG1_OST)
+        delay(10);
 
-  _ctrl_reg1.bit.ALT = 0;
-  write8(MPL3115A2_CTRL_REG1, _ctrl_reg1.reg);
+    _ctrl_reg1.bit.ALT = 0;
+    write8(MPL3115A2_CTRL_REG1, _ctrl_reg1.reg);
 
-  _ctrl_reg1.bit.OST = 1;
-  write8(MPL3115A2_CTRL_REG1, _ctrl_reg1.reg);
+    _ctrl_reg1.bit.OST = 1;
+    write8(MPL3115A2_CTRL_REG1, _ctrl_reg1.reg);
 
-  uint8_t sta = 0;
-  while (!(sta & MPL3115A2_REGISTER_STATUS_PDR)) {
-    sta = read8(MPL3115A2_REGISTER_STATUS);
-    delay(10);
-  }
-  _i2c->beginTransmission(MPL3115A2_ADDRESS); // start transmission to device
-  _i2c->write(MPL3115A2_REGISTER_PRESSURE_MSB);
-  _i2c->endTransmission(false); // end transmission
+    uint8_t sta = 0;
+    while (!(sta & MPL3115A2_REGISTER_STATUS_PDR)) {
+        sta = read8(MPL3115A2_REGISTER_STATUS);
+        delay(10);
+    }
+    _i2c->beginTransmission(MPL3115A2_ADDRESS); // start transmission to device
+    _i2c->write(MPL3115A2_REGISTER_PRESSURE_MSB);
+    _i2c->endTransmission(false); // end transmission
 
-  _i2c->requestFrom((uint8_t)MPL3115A2_ADDRESS,
-                    (uint8_t)3); // send data n-bytes read
-  pressure = _i2c->read();       // receive DATA
-  pressure <<= 8;
-  pressure |= _i2c->read(); // receive DATA
-  pressure <<= 8;
-  pressure |= _i2c->read(); // receive DATA
-  pressure >>= 4;
+    _i2c->requestFrom((uint8_t)MPL3115A2_ADDRESS,
+                      (uint8_t) 3); // send data n-bytes read
+    pressure = _i2c->read();       // receive DATA
+    pressure <<= 8;
+    pressure |= _i2c->read(); // receive DATA
+    pressure <<= 8;
+    pressure |= _i2c->read(); // receive DATA
+    pressure >>= 4;
 
-  float baro = pressure;
-  baro /= 4.0;
-  return baro;
+    float baro = pressure;
+    baro /= 4.0;
+    return baro;
 }
 
 /*!
@@ -118,35 +118,35 @@ float Adafruit_MPL3115A2::getPressure() {
  *  @return altitude reading as a floating-point value
  */
 float Adafruit_MPL3115A2::getAltitude() {
-  int32_t alt;
+    int32_t alt;
 
-  while (read8(MPL3115A2_CTRL_REG1) & MPL3115A2_CTRL_REG1_OST)
-    delay(10);
+    while (read8(MPL3115A2_CTRL_REG1) & MPL3115A2_CTRL_REG1_OST)
+        delay(10);
 
-  _ctrl_reg1.bit.ALT = 1;
-  write8(MPL3115A2_CTRL_REG1, _ctrl_reg1.reg);
+    _ctrl_reg1.bit.ALT = 1;
+    write8(MPL3115A2_CTRL_REG1, _ctrl_reg1.reg);
 
-  _ctrl_reg1.bit.OST = 1;
-  write8(MPL3115A2_CTRL_REG1, _ctrl_reg1.reg);
+    _ctrl_reg1.bit.OST = 1;
+    write8(MPL3115A2_CTRL_REG1, _ctrl_reg1.reg);
 
-  uint8_t sta = 0;
-  while (!(sta & MPL3115A2_REGISTER_STATUS_PDR)) {
-    sta = read8(MPL3115A2_REGISTER_STATUS);
-    delay(10);
-  }
-  _i2c->beginTransmission(MPL3115A2_ADDRESS); // start transmission to device
-  _i2c->write(MPL3115A2_REGISTER_PRESSURE_MSB);
-  _i2c->endTransmission(false); // end transmission
+    uint8_t sta = 0;
+    while (!(sta & MPL3115A2_REGISTER_STATUS_PDR)) {
+        sta = read8(MPL3115A2_REGISTER_STATUS);
+        delay(10);
+    }
+    _i2c->beginTransmission(MPL3115A2_ADDRESS); // start transmission to device
+    _i2c->write(MPL3115A2_REGISTER_PRESSURE_MSB);
+    _i2c->endTransmission(false); // end transmission
 
-  _i2c->requestFrom((uint8_t)MPL3115A2_ADDRESS,
-                    (uint8_t)3);         // send data n-bytes read
-  alt = ((uint32_t)_i2c->read()) << 24;  // receive DATA
-  alt |= ((uint32_t)_i2c->read()) << 16; // receive DATA
-  alt |= ((uint32_t)_i2c->read()) << 8;  // receive DATA
+    _i2c->requestFrom((uint8_t)MPL3115A2_ADDRESS,
+                      (uint8_t) 3);         // send data n-bytes read
+    alt = ((uint32_t) _i2c->read()) << 24;  // receive DATA
+    alt |= ((uint32_t) _i2c->read()) << 16; // receive DATA
+    alt |= ((uint32_t) _i2c->read()) << 8;  // receive DATA
 
-  float altitude = alt;
-  altitude /= 65536.0;
-  return altitude;
+    float altitude = alt;
+    altitude /= 65536.0;
+    return altitude;
 }
 
 /*!
@@ -154,12 +154,12 @@ float Adafruit_MPL3115A2::getAltitude() {
  *  @param pascal the pressure to use as the baseline
  */
 void Adafruit_MPL3115A2::setSeaPressure(float pascal) {
-  uint16_t bar = pascal / 2;
-  _i2c->beginTransmission(MPL3115A2_ADDRESS);
-  _i2c->write((uint8_t)MPL3115A2_BAR_IN_MSB);
-  _i2c->write((uint8_t)(bar >> 8));
-  _i2c->write((uint8_t)bar);
-  _i2c->endTransmission(false);
+    uint16_t bar = pascal / 2;
+    _i2c->beginTransmission(MPL3115A2_ADDRESS);
+    _i2c->write((uint8_t) MPL3115A2_BAR_IN_MSB);
+    _i2c->write((uint8_t)(bar >> 8));
+    _i2c->write((uint8_t) bar);
+    _i2c->endTransmission(false);
 }
 
 /*!
@@ -167,34 +167,34 @@ void Adafruit_MPL3115A2::setSeaPressure(float pascal) {
  *  @return temperature reading in Centigrade as a floating-point value
  */
 float Adafruit_MPL3115A2::getTemperature() {
-  int16_t t;
+    int16_t t;
 
-  _ctrl_reg1.bit.OST = 1;
-  write8(MPL3115A2_CTRL_REG1, _ctrl_reg1.reg);
+    _ctrl_reg1.bit.OST = 1;
+    write8(MPL3115A2_CTRL_REG1, _ctrl_reg1.reg);
 
-  uint8_t sta = 0;
-  while (!(sta & MPL3115A2_REGISTER_STATUS_TDR)) {
-    sta = read8(MPL3115A2_REGISTER_STATUS);
-    delay(10);
-  }
-  _i2c->beginTransmission(MPL3115A2_ADDRESS); // start transmission to device
-  _i2c->write(MPL3115A2_REGISTER_TEMP_MSB);
-  _i2c->endTransmission(false); // end transmission
+    uint8_t sta = 0;
+    while (!(sta & MPL3115A2_REGISTER_STATUS_TDR)) {
+        sta = read8(MPL3115A2_REGISTER_STATUS);
+        delay(10);
+    }
+    _i2c->beginTransmission(MPL3115A2_ADDRESS); // start transmission to device
+    _i2c->write(MPL3115A2_REGISTER_TEMP_MSB);
+    _i2c->endTransmission(false); // end transmission
 
-  _i2c->requestFrom((uint8_t)MPL3115A2_ADDRESS,
-                    (uint8_t)2); // send data n-bytes read
-  t = _i2c->read();              // receive DATA
-  t <<= 8;
-  t |= _i2c->read(); // receive DATA
-  t >>= 4;
+    _i2c->requestFrom((uint8_t)MPL3115A2_ADDRESS,
+                      (uint8_t) 2); // send data n-bytes read
+    t = _i2c->read();              // receive DATA
+    t <<= 8;
+    t |= _i2c->read(); // receive DATA
+    t >>= 4;
 
-  if (t & 0x800) {
-    t |= 0xF000;
-  }
+    if (t & 0x800) {
+        t |= 0xF000;
+    }
 
-  float temp = t;
-  temp /= 16.0;
-  return temp;
+    float temp = t;
+    temp /= 16.0;
+    return temp;
 }
 
 
@@ -206,13 +206,13 @@ float Adafruit_MPL3115A2::getTemperature() {
  *  @return the read data byte
  */
 uint8_t Adafruit_MPL3115A2::read8(uint8_t a) {
-  _i2c->beginTransmission(MPL3115A2_ADDRESS); // start transmission to device
-  _i2c->write(a);               // sends register address to read from
-  _i2c->endTransmission(false); // end transmission
+    _i2c->beginTransmission(MPL3115A2_ADDRESS); // start transmission to device
+    _i2c->write(a);               // sends register address to read from
+    _i2c->endTransmission(false); // end transmission
 
-  _i2c->requestFrom((uint8_t)MPL3115A2_ADDRESS,
-                    (uint8_t)1); // send data n-bytes read
-  return _i2c->read();           // receive DATA
+    _i2c->requestFrom((uint8_t)MPL3115A2_ADDRESS,
+                      (uint8_t) 1); // send data n-bytes read
+    return _i2c->read();           // receive DATA
 }
 
 /*!
@@ -223,10 +223,10 @@ uint8_t Adafruit_MPL3115A2::read8(uint8_t a) {
  *          the byte to write
  */
 void Adafruit_MPL3115A2::write8(uint8_t a, uint8_t d) {
-  _i2c->beginTransmission(MPL3115A2_ADDRESS); // start transmission to device
-  _i2c->write(a);               // sends register address to write to
-  _i2c->write(d);               // sends register data
-  _i2c->endTransmission(false); // end transmission
+    _i2c->beginTransmission(MPL3115A2_ADDRESS); // start transmission to device
+    _i2c->write(a);               // sends register address to write to
+    _i2c->write(d);               // sends register data
+    _i2c->endTransmission(false); // end transmission
 }
 
 
@@ -237,8 +237,8 @@ void Adafruit_MPL3115A2::write8(uint8_t a, uint8_t d) {
 	    where 0 = uninitialised and >100 implies data has been refresehd since reset
 */
 /**************************************************************************/
-unsigned int Adafruit_MPL3115A2::getstate(){
-  return state;
+unsigned int Adafruit_MPL3115A2::getstate() {
+    return state;
 }
 
 /**************************************************************************/
@@ -249,8 +249,8 @@ unsigned int Adafruit_MPL3115A2::getstate(){
 */
 /**************************************************************************/
 void Adafruit_MPL3115A2::setState(unsigned int state) {
-  this->state = state;
-  }
+    this->state = state;
+}
 
 
 /**************************************************************************/
@@ -260,7 +260,7 @@ void Adafruit_MPL3115A2::setState(unsigned int state) {
 */
 /**************************************************************************/
 float Adafruit_MPL3115A2::getPressureAsync() {
-  return baro;
+    return baro;
 }
 /**************************************************************************/
 /*!
@@ -269,7 +269,7 @@ float Adafruit_MPL3115A2::getPressureAsync() {
 */
 /**************************************************************************/
 float Adafruit_MPL3115A2::getTemperatureAsync() {
-  return temp;
+    return temp;
 }
 /**************************************************************************/
 /*!
@@ -278,7 +278,7 @@ float Adafruit_MPL3115A2::getTemperatureAsync() {
 */
 /**************************************************************************/
 float Adafruit_MPL3115A2::getAltitudeAsync() {
-  return altitude;
+    return altitude;
 }
 /**************************************************************************/
 /*!
@@ -287,7 +287,7 @@ float Adafruit_MPL3115A2::getAltitudeAsync() {
 */
 /**************************************************************************/
 bool Adafruit_MPL3115A2::isNewData() {
-  return (state>=100);
+    return (state >= 100);
 }
 /**************************************************************************/
 /*!
@@ -296,7 +296,7 @@ bool Adafruit_MPL3115A2::isNewData() {
 */
 /**************************************************************************/
 void Adafruit_MPL3115A2::reset(bool reset) {
-  if (reset && state>=100) state-=100;
+    if (reset && state >= 100) state -= 100;
 }
 
 /**************************************************************************/
@@ -306,116 +306,116 @@ void Adafruit_MPL3115A2::reset(bool reset) {
 */
 /**************************************************************************/
 void Adafruit_MPL3115A2::poll(bool quick) {
-  uint32_t pressure;
-  int32_t alt;
-  int16_t t;
-  uint8_t sta;
-  switch (state){
-  default:     // unknown state - assume we aren't initialised
-    state=0;
-    break;
-  case 0:     // we weren't initialised with begin(); do nothing.
-    break;
-  case 1:     // reading pressure   
-  case 101:     // reading pressure   
-    if ((read8(MPL3115A2_CTRL_REG1) & MPL3115A2_CTRL_REG1_OST)) break;
-    state++;
-    if (quick) break;
-  case 2:
-  case 102:
-    _ctrl_reg1.bit.ALT = 0;
-    write8(MPL3115A2_CTRL_REG1, _ctrl_reg1.reg);
-  
-    _ctrl_reg1.bit.OST = 1;
-    write8(MPL3115A2_CTRL_REG1, _ctrl_reg1.reg);
-  
-    state++;
-    if (quick) break;
-  case 3:     // 
-  case 103:     // 
-    sta = read8(MPL3115A2_REGISTER_STATUS);
-    if (!(sta & MPL3115A2_REGISTER_STATUS_PDR)) break;
-    state++;
-    if (quick) break;
-  case 4:
-  case 104:
-    _i2c->beginTransmission(MPL3115A2_ADDRESS); // start transmission to device 
-    _i2c->write(MPL3115A2_REGISTER_PRESSURE_MSB); 
-    _i2c->endTransmission(false); // end transmission
-    
-    _i2c->requestFrom((uint8_t)MPL3115A2_ADDRESS, (uint8_t)3);// send data n-bytes read
-    pressure = _i2c->read(); // receive DATA
-    pressure <<= 8;
-    pressure |= _i2c->read(); // receive DATA
-    pressure <<= 8;
-    pressure |= _i2c->read(); // receive DATA
-    pressure >>= 4;
-  
-    baro = pressure;
-    baro /= 4.0;
-    state++;
-    if (quick) break;
-  case 5:     // read altitude 
-  case 105:     // read altitude 
-    if ((read8(MPL3115A2_CTRL_REG1) & MPL3115A2_CTRL_REG1_OST)) break;
-    state++;
-    if (quick) break;
-  case 6:
-  case 106:
-    _ctrl_reg1.bit.ALT = 1;
-    write8(MPL3115A2_CTRL_REG1, _ctrl_reg1.reg);
-  
-    _ctrl_reg1.bit.OST = 1;
-    write8(MPL3115A2_CTRL_REG1, _ctrl_reg1.reg);
-  
-    state++;
-    if (quick) break;
-  case 7:
-  case 107:
-    sta = read8(MPL3115A2_REGISTER_STATUS);
-    if (! (sta & MPL3115A2_REGISTER_STATUS_PDR)) break;
-    state++;
-    if (!quick) break;
-  case 8:
-  case 108:
-    _i2c->beginTransmission(MPL3115A2_ADDRESS); // start transmission to device 
-    _i2c->write(MPL3115A2_REGISTER_PRESSURE_MSB); 
-    _i2c->endTransmission(false); // end transmission
-    
-    _i2c->requestFrom((uint8_t)MPL3115A2_ADDRESS, (uint8_t)3);// send data n-bytes read
-    alt  = ((uint32_t)_i2c->read()) << 24; // receive DATA
-    alt |= ((uint32_t)_i2c->read()) << 16; // receive DATA
-    alt |= ((uint32_t)_i2c->read()) << 8; // receive DATA
-  
-    altitude = alt;
-    altitude /= 65536.0;
-    state++;
-    if (quick) break;
-  case 9:     // read temperature
-  case 109:     // read temperature
-    sta = read8(MPL3115A2_REGISTER_STATUS);
-    if (! (sta & MPL3115A2_REGISTER_STATUS_TDR)) break;
-    state++;
-    if (quick) break;
-  case 10:
-  case 110:
-    _i2c->beginTransmission(MPL3115A2_ADDRESS); // start transmission to device 
-    _i2c->write(MPL3115A2_REGISTER_TEMP_MSB); 
-    _i2c->endTransmission(false); // end transmission
-    
-    _i2c->requestFrom((uint8_t)MPL3115A2_ADDRESS, (uint8_t)2);// send data n-bytes read
-    t = _i2c->read(); // receive DATA
-    t <<= 8;
-    t |= _i2c->read(); // receive DATA
-    t >>= 4;
-    
-    if (t & 0x800) {
-      t |= 0xF000;
+    uint32_t pressure;
+    int32_t alt;
+    int16_t t;
+    uint8_t sta;
+    switch (state) {
+        default:     // unknown state - assume we aren't initialised
+            state = 0;
+            break;
+        case 0:     // we weren't initialised with begin(); do nothing.
+            break;
+        case 1:     // reading pressure
+        case 101:     // reading pressure
+            if ((read8(MPL3115A2_CTRL_REG1) & MPL3115A2_CTRL_REG1_OST)) break;
+            state++;
+            if (quick) break;
+        case 2:
+        case 102:
+            _ctrl_reg1.bit.ALT = 0;
+            write8(MPL3115A2_CTRL_REG1, _ctrl_reg1.reg);
+
+            _ctrl_reg1.bit.OST = 1;
+            write8(MPL3115A2_CTRL_REG1, _ctrl_reg1.reg);
+
+            state++;
+            if (quick) break;
+        case 3:     //
+        case 103:     //
+            sta = read8(MPL3115A2_REGISTER_STATUS);
+            if (!(sta & MPL3115A2_REGISTER_STATUS_PDR)) break;
+            state++;
+            if (quick) break;
+        case 4:
+        case 104:
+            _i2c->beginTransmission(MPL3115A2_ADDRESS); // start transmission to device
+            _i2c->write(MPL3115A2_REGISTER_PRESSURE_MSB);
+            _i2c->endTransmission(false); // end transmission
+
+            _i2c->requestFrom((uint8_t)MPL3115A2_ADDRESS, (uint8_t) 3);// send data n-bytes read
+            pressure = _i2c->read(); // receive DATA
+            pressure <<= 8;
+            pressure |= _i2c->read(); // receive DATA
+            pressure <<= 8;
+            pressure |= _i2c->read(); // receive DATA
+            pressure >>= 4;
+
+            baro = pressure;
+            baro /= 4.0;
+            state++;
+            if (quick) break;
+        case 5:     // read altitude
+        case 105:     // read altitude
+            if ((read8(MPL3115A2_CTRL_REG1) & MPL3115A2_CTRL_REG1_OST)) break;
+            state++;
+            if (quick) break;
+        case 6:
+        case 106:
+            _ctrl_reg1.bit.ALT = 1;
+            write8(MPL3115A2_CTRL_REG1, _ctrl_reg1.reg);
+
+            _ctrl_reg1.bit.OST = 1;
+            write8(MPL3115A2_CTRL_REG1, _ctrl_reg1.reg);
+
+            state++;
+            if (quick) break;
+        case 7:
+        case 107:
+            sta = read8(MPL3115A2_REGISTER_STATUS);
+            if (!(sta & MPL3115A2_REGISTER_STATUS_PDR)) break;
+            state++;
+            if (!quick) break;
+        case 8:
+        case 108:
+            _i2c->beginTransmission(MPL3115A2_ADDRESS); // start transmission to device
+            _i2c->write(MPL3115A2_REGISTER_PRESSURE_MSB);
+            _i2c->endTransmission(false); // end transmission
+
+            _i2c->requestFrom((uint8_t)MPL3115A2_ADDRESS, (uint8_t) 3);// send data n-bytes read
+            alt = ((uint32_t) _i2c->read()) << 24; // receive DATA
+            alt |= ((uint32_t) _i2c->read()) << 16; // receive DATA
+            alt |= ((uint32_t) _i2c->read()) << 8; // receive DATA
+
+            altitude = alt;
+            altitude /= 65536.0;
+            state++;
+            if (quick) break;
+        case 9:     // read temperature
+        case 109:     // read temperature
+            sta = read8(MPL3115A2_REGISTER_STATUS);
+            if (!(sta & MPL3115A2_REGISTER_STATUS_TDR)) break;
+            state++;
+            if (quick) break;
+        case 10:
+        case 110:
+            _i2c->beginTransmission(MPL3115A2_ADDRESS); // start transmission to device
+            _i2c->write(MPL3115A2_REGISTER_TEMP_MSB);
+            _i2c->endTransmission(false); // end transmission
+
+            _i2c->requestFrom((uint8_t)MPL3115A2_ADDRESS, (uint8_t) 2);// send data n-bytes read
+            t = _i2c->read(); // receive DATA
+            t <<= 8;
+            t |= _i2c->read(); // receive DATA
+            t >>= 4;
+
+            if (t & 0x800) {
+                t |= 0xF000;
+            }
+
+            temp = t;
+            temp /= 16.0;
+            state = 101;              // state >100 implies new data
+            break;
     }
-  
-    temp = t;
-    temp /= 16.0;
-    state=101;              // state >100 implies new data
-    break;
-  }
 }
--- Adafruit_MPL3115A2.h
+++ Adafruit_MPL3115A2.h
@@ -30,75 +30,75 @@
 
 /** MPL3115A2 registers **/
 enum {
-  MPL3115A2_REGISTER_STATUS = (0x00),
+    MPL3115A2_REGISTER_STATUS = (0x00),
 
-  MPL3115A2_REGISTER_PRESSURE_MSB = (0x01),
-  MPL3115A2_REGISTER_PRESSURE_CSB = (0x02),
-  MPL3115A2_REGISTER_PRESSURE_LSB = (0x03),
+    MPL3115A2_REGISTER_PRESSURE_MSB = (0x01),
+    MPL3115A2_REGISTER_PRESSURE_CSB = (0x02),
+    MPL3115A2_REGISTER_PRESSURE_LSB = (0x03),
 
-  MPL3115A2_REGISTER_TEMP_MSB = (0x04),
-  MPL3115A2_REGISTER_TEMP_LSB = (0x05),
+    MPL3115A2_REGISTER_TEMP_MSB = (0x04),
+    MPL3115A2_REGISTER_TEMP_LSB = (0x05),
 
-  MPL3115A2_REGISTER_DR_STATUS = (0x06),
+    MPL3115A2_REGISTER_DR_STATUS = (0x06),
 
-  MPL3115A2_OUT_P_DELTA_MSB = (0x07),
-  MPL3115A2_OUT_P_DELTA_CSB = (0x08),
-  MPL3115A2_OUT_P_DELTA_LSB = (0x09),
+    MPL3115A2_OUT_P_DELTA_MSB = (0x07),
+    MPL3115A2_OUT_P_DELTA_CSB = (0x08),
+    MPL3115A2_OUT_P_DELTA_LSB = (0x09),
 
-  MPL3115A2_OUT_T_DELTA_MSB = (0x0A),
-  MPL3115A2_OUT_T_DELTA_LSB = (0x0B),
+    MPL3115A2_OUT_T_DELTA_MSB = (0x0A),
+    MPL3115A2_OUT_T_DELTA_LSB = (0x0B),
 
-  MPL3115A2_WHOAMI = (0x0C),
+    MPL3115A2_WHOAMI = (0x0C),
 
-  MPL3115A2_BAR_IN_MSB = (0x14),
-  MPL3115A2_BAR_IN_LSB = (0x15),
+    MPL3115A2_BAR_IN_MSB = (0x14),
+    MPL3115A2_BAR_IN_LSB = (0x15),
 };
 
 /** MPL3115A2 status register bits **/
 enum {
-  MPL3115A2_REGISTER_STATUS_TDR = 0x02,
-  MPL3115A2_REGISTER_STATUS_PDR = 0x04,
-  MPL3115A2_REGISTER_STATUS_PTDR = 0x08,
+    MPL3115A2_REGISTER_STATUS_TDR = 0x02,
+    MPL3115A2_REGISTER_STATUS_PDR = 0x04,
+    MPL3115A2_REGISTER_STATUS_PTDR = 0x08,
 };
 
 /** MPL3115A2 PT DATA register bits **/
 enum {
-  MPL3115A2_PT_DATA_CFG = 0x13,
-  MPL3115A2_PT_DATA_CFG_TDEFE = 0x01,
-  MPL3115A2_PT_DATA_CFG_PDEFE = 0x02,
-  MPL3115A2_PT_DATA_CFG_DREM = 0x04,
+    MPL3115A2_PT_DATA_CFG = 0x13,
+    MPL3115A2_PT_DATA_CFG_TDEFE = 0x01,
+    MPL3115A2_PT_DATA_CFG_PDEFE = 0x02,
+    MPL3115A2_PT_DATA_CFG_DREM = 0x04,
 };
 
 /** MPL3115A2 control registers **/
 enum {
 
-  MPL3115A2_CTRL_REG1 = (0x26),
-  MPL3115A2_CTRL_REG2 = (0x27),
-  MPL3115A2_CTRL_REG3 = (0x28),
-  MPL3115A2_CTRL_REG4 = (0x29),
-  MPL3115A2_CTRL_REG5 = (0x2A),
+    MPL3115A2_CTRL_REG1 = (0x26),
+    MPL3115A2_CTRL_REG2 = (0x27),
+    MPL3115A2_CTRL_REG3 = (0x28),
+    MPL3115A2_CTRL_REG4 = (0x29),
+    MPL3115A2_CTRL_REG5 = (0x2A),
 };
 
 /** MPL3115A2 control register bits **/
 enum {
-  MPL3115A2_CTRL_REG1_SBYB = 0x01,
-  MPL3115A2_CTRL_REG1_OST = 0x02,
-  MPL3115A2_CTRL_REG1_RST = 0x04,
-  MPL3115A2_CTRL_REG1_RAW = 0x40,
-  MPL3115A2_CTRL_REG1_ALT = 0x80,
-  MPL3115A2_CTRL_REG1_BAR = 0x00,
+    MPL3115A2_CTRL_REG1_SBYB = 0x01,
+    MPL3115A2_CTRL_REG1_OST = 0x02,
+    MPL3115A2_CTRL_REG1_RST = 0x04,
+    MPL3115A2_CTRL_REG1_RAW = 0x40,
+    MPL3115A2_CTRL_REG1_ALT = 0x80,
+    MPL3115A2_CTRL_REG1_BAR = 0x00,
 };
 
 /** MPL3115A2 oversample values **/
 enum {
-  MPL3115A2_CTRL_REG1_OS1 = 0x00,
-  MPL3115A2_CTRL_REG1_OS2 = 0x08,
-  MPL3115A2_CTRL_REG1_OS4 = 0x10,
-  MPL3115A2_CTRL_REG1_OS8 = 0x18,
-  MPL3115A2_CTRL_REG1_OS16 = 0x20,
-  MPL3115A2_CTRL_REG1_OS32 = 0x28,
-  MPL3115A2_CTRL_REG1_OS64 = 0x30,
-  MPL3115A2_CTRL_REG1_OS128 = 0x38,
+    MPL3115A2_CTRL_REG1_OS1 = 0x00,
+    MPL3115A2_CTRL_REG1_OS2 = 0x08,
+    MPL3115A2_CTRL_REG1_OS4 = 0x10,
+    MPL3115A2_CTRL_REG1_OS8 = 0x18,
+    MPL3115A2_CTRL_REG1_OS16 = 0x20,
+    MPL3115A2_CTRL_REG1_OS32 = 0x28,
+    MPL3115A2_CTRL_REG1_OS64 = 0x30,
+    MPL3115A2_CTRL_REG1_OS128 = 0x38,
 };
 
 #define MPL3115A2_REGISTER_STARTCONVERSION (0x12) ///< start conversion
@@ -109,47 +109,60 @@ enum {
  */
 class Adafruit_MPL3115A2 {
 public:
-  Adafruit_MPL3115A2();
-  boolean begin(TwoWire *twoWire = &Wire);
-  float getPressure(void);
-  float getAltitude(void);
-  float getTemperature(void);
-  void setSeaPressure(float pascal);
+    Adafruit_MPL3115A2();
 
-  void write8(uint8_t a, uint8_t d);
+    boolean begin(TwoWire *twoWire = &Wire);
 
-  float getPressureAsync(void);
-  float getAltitudeAsync(void);
-  float getTemperatureAsync(void);
-  void poll(bool quick = true);
-  
-  unsigned int getstate();
-  void setState(unsigned int state);
-  bool isNewData();
-  void reset(bool reset=true);
+    float getPressure(void);
 
- private:
-  TwoWire *_i2c;
-  uint8_t read8(uint8_t a);
-  uint8_t mode;
+    float getAltitude(void);
 
-  float baro=NAN;
-  float temp=NAN;
-  float altitude=NAN;
-  uint8_t state=0;
+    float getTemperature(void);
 
-  typedef union {
-    struct {
-      uint8_t SBYB : 1;
-      uint8_t OST : 1;
-      uint8_t RST : 1;
-      uint8_t OS : 3;
-      uint8_t RAW : 1;
-      uint8_t ALT : 1;
-    } bit;
-    uint8_t reg;
-  } ctrl_reg1;
-  ctrl_reg1 _ctrl_reg1;
+    void setSeaPressure(float pascal);
+
+    void write8(uint8_t a, uint8_t d);
+
+    float getPressureAsync(void);
+
+    float getAltitudeAsync(void);
+
+    float getTemperatureAsync(void);
+
+    void poll(bool quick = true);
+
+    unsigned int getstate();
+
+    void setState(unsigned int state);
+
+    bool isNewData();
+
+    void reset(bool reset = true);
+
+private:
+    TwoWire *_i2c;
+
+    uint8_t read8(uint8_t a);
+
+    uint8_t mode;
+
+    float baro = NAN;
+    float temp = NAN;
+    float altitude = NAN;
+    uint8_t state = 0;
+
+    typedef union {
+        struct {
+            uint8_t SBYB: 1;
+            uint8_t OST: 1;
+            uint8_t RST: 1;
+            uint8_t OS: 3;
+            uint8_t RAW: 1;
+            uint8_t ALT: 1;
+        } bit;
+        uint8_t reg;
+    } ctrl_reg1;
+    ctrl_reg1 _ctrl_reg1;
 };
 
 #endif
